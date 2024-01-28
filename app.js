87cd97e4841b0de74b223a14760b94bb80872bbe
ElimpeJunior <133907272+ElimpeJunior@users.noreply.github.com>
2024-01-28 18:05:41 +0100

    Added the merchant option and fixed it

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 function call() {
   var receiver = document.querySelector("input[name=tel]").value;
   var amount = document.querySelector("input[name=number]").value;
+  var merchant = document.querySelector("input[name=merchant]").value;
   localStorage.setItem("receiver", receiver);
   localStorage.setItem("amount", amount);
+  localStorage.setItem("merchant", merchant);
 
-if (!receiver || !amount) {
+  if (!amount) {
     return;
-}
-localStorage.setItem("receiver", receiver);
-localStorage.setItem("amount", amount);
+  }
 
-var ussd = `*126*9*${receiver}*${amount}%23`;
+  var ussd;
+  if (merchant) {
+    ussd = `*126*4*${merchant}*${amount}%23`;
+  } else if (receiver) {
+    ussd = `*126*9*${receiver}*${amount}%23`;
+  } else {
+    return;
+  }
 
-window.open(`tel:${ussd}`);
+  window.location.href = `tel:${ussd}`;
 }
 
 function initialize() {
-  document.querySelector("#pay-btn").addEventListener("click", call);
+  document
+    .querySelector("#pay-btn")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent form submission
+      call();
+    });
 
   var storedAmount = localStorage.getItem("amount");
   document.getElementById("amount").value = storedAmount;
@@ -27,7 +39,3 @@ function initialize() {
   var storedMerchant = localStorage.getItem("merchant");
   document.getElementById("merchant").value = storedMerchant;
 }
-  
-
-
-
